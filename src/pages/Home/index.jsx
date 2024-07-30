95a625e64edda7b0c177a22bd1b05110de388565
VTEXsx <8221vitor@gmail.com>
2024-07-30 15:40:00 -0300

    Home criada, adicionado  o nome do usuario que fez login com mensagem de bem vindo, botão de logout, botão de deletar a conta e responsidade

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      setName(user.name);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    navigate("/");
+  };
+  const handleDeleteAccount = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  }
+
+  return (
+    <main className="bg-custom-bg w-screen h-screen flex flex-col items-center justify-center gap-12">
+      <h1 className="text-3xl text-white px-4 md:px-20 text-center font-bold">
+        Bem-vindo ao Frontend Fusion, {name}!
+      </h1>
+
+      <div className="flex gap-5 text-base">
+        <button
+          onClick={handleLogout}
+          className="bg-teal-500 hover:bg-teal-700 text-white/90 font-bold py-2 w-40 rounded-md "
+        >
+          Sair
+        </button>
+        <button
+          onClick={handleDeleteAccount}
+          className="bg-red-500 hover:bg-red-700 text-white/90 font-bold py-2 w-40 rounded-md "
+        >
+          Apagar conta
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
