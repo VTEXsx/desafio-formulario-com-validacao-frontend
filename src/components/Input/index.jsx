9fed5f845b70871ae932bd38e5115e7d6b6db8e0
VTEXsx <8221vitor@gmail.com>
2024-07-29 21:42:11 -0300

    responsividade da tela de login concluida, irei criar componente do container de formulario ja que utilizo o mesmo container em duas paginas

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 
-function Input({ tipo, text, ids, register }) {
+function Input({ type, text, id, register }) {
   return (
     <>
-      <label className="text-white/50 font-bold mb-2" htmlFor={ids}>
+      <label className="text-white/50 font-bold mb-2" htmlFor={id}>
         {text}
       </label>
       <input
-        className="px-3 py-2 border-[1px] border-white/30 rounded-md"
-        type={tipo}
-        id={ids}
+        className="px-2 lg:py-1 py-2 border-[1px] border-white/30 rounded-md"
+        type={type}
+        id={id}
         {...register}
       />
     </>
@@ -17,9 +17,9 @@ function Input({ tipo, text, ids, register }) {
 }
 
 Input.propTypes = {
-  tipo: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  ids: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
   register: PropTypes.object.isRequired,
 };
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,24 +1,33 @@
 import { Link } from "react-router-dom";
-import Input from "../../components/Input";
 import BtnSubmit from "../../components/BtnSubmit";
+import Input from "../../components/Input";
 
 function Login() {
- return (
-   <main className="bg-custom-bg w-screen h-screen">
-     <div className="bg-white/10 border-[1px] border-white/15 w-full h-full flex flex-col items-center justify-center space-y-4">
-       <h1 className="text-teal-400 font-bold text-3xl text-center">
-         FRONTEND <br /> FUSION
-       </h1>
+  return (
+    <main className="bg-custom-bg w-screen h-screen flex items-center justify-center py-5">
+      <div className="bg-white/5 flex flex-col items-center justify-center space-y-4 md:space-y-8 md:rounded-xl py-48 md:py-14 lg:py-8  w-screen md:w-3/5 lg:w-2/6 xl:w-1/4">
+        <h1 className="text-teal-400 font-bold text-3xl md:text-5xl lg:text-4xl text-center">
+          FRONTEND <br /> FUSION
+        </h1>
 
-       <form className="w-full space-y-7 px-4 flex flex-col items-center">
-         <Input type="email" label="E-mail" />
-         <Input type="password" label="Senha"/>
-         <BtnSubmit>Entrar</BtnSubmit>
-         <p className="text-white">Ainda não tem conta? <Link className="text-teal-500" to="/register">Cadastre-se</Link></p>
-       </form>
-     </div>
-   </main>
- );
+        <form className="w-full space-y-7 px-6 md:px-10  lg:px-6 flex flex-col items-center md:text-lg lg:text-base">
+          <div className="flex flex-col w-full">
+            <Input type="email" text="E-mail" />
+          </div>
+          <div className="flex flex-col w-full">
+            <Input type="password" text="Senha" />
+          </div>
+          <BtnSubmit>Entrar</BtnSubmit>
+          <p className="text-white">
+            Ainda não tem conta?{" "}
+            <Link className="text-teal-500" to="/register">
+              Cadastre-se
+            </Link>
+          </p>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default Login;
